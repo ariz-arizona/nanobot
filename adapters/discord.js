78fe673555c345f39ac96373137b38d03495c563
Xenia Mihaqlova <ar.ariz.arizona@gmail.com>
2022-08-13 23:57:08 +0300

    try test

--- adapters/discord.js
+++ adapters/discord.js
@@ -86,109 +86,148 @@ const getFreeDates = async (username) => {
 };
 
 router.post("/bot_stat", async (_req, res) => {
-  const message = _req.body;
-  const { token, userId } = message;
+  try {
+    const message = _req.body;
+    const { token, userId } = message;
 
-  const data = await getStat(userId);
-  const text =
-    data.length > 1
-      ? [
-          `Запрос от пользователя: ${userId}`,
-          `Пользователь: ${data[0]}`,
-          `Цель: ${data[1]}`,
-          `Среднее: ${data[data.length - 3]}`,
-          `Общее: ${data[data.length - 2]}`,
-          `Остаток до цели: ${data[data.length - 1]}`,
-        ]
-      : [
-          `Ничего не найдено для ${
-            typeof userId === "number" ? `айди ` : `пользователя`
-          } ${userId}`,
-        ];
+    const data = await getStat(userId);
+    const text =
+      data.length > 1
+        ? [
+            `Запрос от пользователя: ${userId}`,
+            `Пользователь: ${data[0]}`,
+            `Цель: ${data[1]}`,
+            `Среднее: ${data[data.length - 3]}`,
+            `Общее: ${data[data.length - 2]}`,
+            `Остаток до цели: ${data[data.length - 1]}`,
+          ]
+        : [
+            `Ничего не найдено для ${
+              typeof userId === "number" ? `айди ` : `пользователя`
+            } ${userId}`,
+          ];
 
-  await fetch(
-    `https://discord.com/api/v8/webhooks/${DISCORD_APPLICATION_ID}/${token}`,
-    {
-      headers: { "Content-Type": "application/json" },
-      method: "POST",
-      body: JSON.stringify({
-        content: text.join("\n"),
-      }),
-    }
-  );
-
+    await fetch(
+      `https://discord.com/api/v8/webhooks/${DISCORD_APPLICATION_ID}/${token}`,
+      {
+        headers: { "Content-Type": "application/json" },
+        method: "POST",
+        body: JSON.stringify({
+          content: text.join("\n"),
+        }),
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    await fetch(
+      `https://discord.com/api/v8/webhooks/${DISCORD_APPLICATION_ID}/${message.token}`,
+      {
+        headers: { "Content-Type": "application/json" },
+        method: "post",
+        body: JSON.stringify({
+          content: errorMessage(error),
+        }),
+      }
+    );
+  }
   res.sendStatus(200);
 });
 
 router.post("/bot_add", async (_req, res) => {
-  const message = _req.body;
-  const { token, words, username } = message;
+  try {
+    const message = _req.body;
+    const { token, words, username } = message;
 
-  const freeDates = await getFreeDates(username);
-  freeDates[0].focused = true;
-  freeDates.map((el) => (el.value = [el.value, words].join("_")));
+    const freeDates = await getFreeDates(username);
+    freeDates[0].focused = true;
+    freeDates.map((el) => (el.value = [el.value, words].join("_")));
 
-  await fetch(
-    `https://discord.com/api/v8/webhooks/${DISCORD_APPLICATION_ID}/${token}`,
-    {
-      headers: { "Content-Type": "application/json" },
-      method: "POST",
-      body: JSON.stringify({
-        content: `Пользователь ${username} готов вписать слова (${words}) в дату`,
-        components: [
-          {
-            type: 1,
-            components: [
-              {
-                type: 3,
-                custom_id: "free_date",
-                options: freeDates,
-                min_values: 1,
-                max_values: 1,
-                autocomplete: true,
-              },
-            ],
-          },
-        ],
-      }),
-    }
-  );
-
+    await fetch(
+      `https://discord.com/api/v8/webhooks/${DISCORD_APPLICATION_ID}/${token}`,
+      {
+        headers: { "Content-Type": "application/json" },
+        method: "POST",
+        body: JSON.stringify({
+          content: `Пользователь ${username} готов вписать слова (${words}) в дату`,
+          components: [
+            {
+              type: 1,
+              components: [
+                {
+                  type: 3,
+                  custom_id: "free_date",
+                  options: freeDates,
+                  min_values: 1,
+                  max_values: 1,
+                  autocomplete: true,
+                },
+              ],
+            },
+          ],
+        }),
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    await fetch(
+      `https://discord.com/api/v8/webhooks/${DISCORD_APPLICATION_ID}/${message.token}`,
+      {
+        headers: { "Content-Type": "application/json" },
+        method: "post",
+        body: JSON.stringify({
+          content: errorMessage(error),
+        }),
+      }
+    );
+  }
   res.sendStatus(200);
 });
 
 router.post("/bot_add_two", async (_req, res) => {
-  const message = _req.body;
-  const { token, date, words, username } = message;
+  try {
+    const message = _req.body;
+    const { token, date, words, username } = message;
 
-  // console.log(date, words, username);
+    // console.log(date, words, username);
 
-  const sheets = google.sheets({
-    version: "v4",
-    auth: GOOGLE_API_KEY,
-  });
+    const sheets = google.sheets({
+      version: "v4",
+      auth: GOOGLE_API_KEY,
+    });
 
-  const jwt = await auth();
+    const jwt = await auth();
 
-  sheets.spreadsheets.values.update({
-    auth: jwt,
-    spreadsheetId: SPREADSHEET_ID,
-    range: `Список участников!${date}`,
-    valueInputOption: "USER_ENTERED",
-    resource: { values: [[words]] },
-  });
+    sheets.spreadsheets.values.update({
+      auth: jwt,
+      spreadsheetId: SPREADSHEET_ID,
+      range: `Список участников!${date}`,
+      valueInputOption: "USER_ENTERED",
+      resource: { values: [[words]] },
+    });
 
-  await fetch(
-    `https://discord.com/api/v8/webhooks/${DISCORD_APPLICATION_ID}/${token}`,
-    {
-      headers: { "Content-Type": "application/json" },
-      method: "POST",
-      body: JSON.stringify({
-        content: `Ячейка обновлена`,
-      }),
-    }
-  );
-
+    await fetch(
+      `https://discord.com/api/v8/webhooks/${DISCORD_APPLICATION_ID}/${token}`,
+      {
+        headers: { "Content-Type": "application/json" },
+        method: "POST",
+        body: JSON.stringify({
+          content: `Ячейка обновлена`,
+        }),
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    await fetch(
+      `https://discord.com/api/v8/webhooks/${DISCORD_APPLICATION_ID}/${message.token}`,
+      {
+        headers: { "Content-Type": "application/json" },
+        method: "post",
+        body: JSON.stringify({
+          content: errorMessage(error),
+        }),
+      }
+    );
+  }
   res.sendStatus(200);
 });
 
